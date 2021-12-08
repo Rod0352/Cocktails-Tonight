40e2b4b96067e75dd905b425c7ef90a6ca16749b
Benjamin Amakye <benjaminamakye@Benjamins-MacBook-Pro.local>
2021-12-08 06:07:01 -0500

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-var ingredients = ["white rum", "sugar syrup", "lime juice", "orange juice", "pineapple juice", "apple juice", "sweet vermouth", "bourbon whiskey", "tequila (repososada)", "pomegrnate", "Egg white", "Orange Bitters", "Cranberry Juice", "Grand Marnier liqueur", 
-"soda(club soda)", "cream", "Blended Scotch", "fresh Mint", "Campri Bitter", "Amaretto Liqueur", "milk", "Black raspberry", "milk", "Runny Hunny", "Benedictine", "Aged Rum", "Cherry brand", "Almond", "Cachaca" ];
+var ingredients = ["white rum", "sugar syrup", "lime juice", "orange juice", "pineapple juice",
+ "apple juice", "sweet vermouth", "bourbon whiskey", "tequila (repososada)", "pomegrnate", "Egg white",
+  "Orange Bitters", "Cranberry Juice", "Grand Marnier liqueur", 
+"soda(club soda)", "cream", "Blended Scotch", "fresh Mint", "Campri Bitter", "Amaretto Liqueur", "milk", 
+"Black raspberry", "milk", "Runny Hunny", "Benedictine", "Aged Rum", "Cherry brand", "Almond", "Cachaca"
+];
 
 $(document).ready(function() {
     $("#dropdown").hide();
