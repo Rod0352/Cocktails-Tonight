1e2d8645ad5e5eb4c8d4e0331600f8257242d98f
Jennifer Emanuele <jemanuele@gmail.com>
2021-12-13 15:45:38 -0500

    API call for second button

--- assets/js/api-call.js
+++ assets/js/api-call.js
@@ -1,14 +1,46 @@
 var getCocktails = function(drinks) {
+    
     //format cocktail api url
-    var apiURL = "www.thecocktaildb.com/api/json/v1/1/random.php";
+    var apiUrlRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
     //request to the API URL
-    fetch(apiURL).then(function(response){
-        response.json().then(function(data) {
-            console.log(data);
+    fetch(apiUrlRandom).then(function(response) {
+        response.json().then(function(res) {
+            console.log(res);
+            jsonData = res;
+            const data = res[0];
+            console.log(res[0]);
         });
     });      
        
 };
 
-getCocktails("random");
+var displayCocktails = function(drinks) {
+    console.log(drinks);
+    console.log(displayCocktails);
+};
+
+// Two ID names on this button??
+//$("#btn-search").click(function() {
+//     var getRandomCocktail = $("#random-cocktail").text();
+//     getCocktails(getRandomCocktails);
+// });
+
+var getDrinkByIngred = function() {
+
+    var apiUrlIngredients ="https:/www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka";
+
+    fetch(apiUrlIngredients).then(function(response){
+        response.json().then(function(res){
+            console.log(res);
+            jsonData = res;
+            const data = res[0];
+            console.log(res[0]);
+        });
+    });
+};
+
+// Not sure about some of the ID names here??
+//$("#custom-btn").click(function() {
+//     var getCustomDrink =$()
+// })
