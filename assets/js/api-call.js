2d54f4d5854c18d61a66aadfc628843e622614a2
Jennifer Emanuele <jemanuele@gmail.com>
2021-12-13 12:34:47 -0500

    API call updated & verified, code working

--- assets/js/api-call.js
+++ assets/js/api-call.js
@@ -1,5 +1,14 @@
-var getCocktails = function() {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+var getCocktails = function(drinks) {
+    //format cocktail api url
+    var apiURL = "www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    //request to the API URL
+    fetch(apiURL).then(function(response){
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });      
+       
 };
 
-getCocktails();
+getCocktails("random");
